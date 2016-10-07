2621fcce8b536078f9ea8ac7b55deea98c8acb8f
Weston Miller <weston.miller@cognizant.com>
2016-10-07 14:27:00 -0600

    time returned formatted

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ String.prototype.paddingLeft = function () {
 
 server.listen(process.env.PORT || 3007);
 
-let eventEndTime = moment('2016-10-07 03:00-06:00');
+let eventEndTime = moment('2016-10-07 15:00-06:00');
 let eventEndTimeSeconds = eventEndTime.valueOf();
 
 io.on('connection', (socket) => {
